634e9f2e94e337c784ef37f920f0502127b6a0ce
Ruthst100 <154247532+Ruthst100@users.noreply.github.com>
2023-12-18 20:26:47 +0200

    Update AddTwo.java
    First Update

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,12 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+	//get 2 numbers from the command-line and define them as a variable of type int
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        // define a new variable "sum" that represents the sum of num1 and num2 
+        int sum = num1+num2;
+        // print
+        System.err.println(num1 + " + " + num2 + " = " + sum);	
 	}
 }
